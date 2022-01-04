fcd79e1982e30d62927f1bd9fa8ede51fb472d3e
Akeju Oluwanifemi <95282550+spyatmycode@users.noreply.github.com>
2022-01-04 10:20:38 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function show(){
+    document.getElementById("bars").style.display= "none";
+    document.getElementById("times").style.display="block";
+    document.getElementById("nav").style.display="flex";
+}
+
+function hide(){
+    document.getElementById("bars").style.display="block";
+    document.getElementById("times").style.display="none";
+    document.getElementById("nav").style.display="none";
+}
+
